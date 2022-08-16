16dce4bea1c97904627052e524db00c4a52cf5c3
Julianah Tomson <85036077+Jullie2024@users.noreply.github.com>
2022-08-16 22:36:40 +0800

    Update syslog-gen.py

--- DataServer/syslog-gen.py
+++ DataServer/syslog-gen.py
@@ -1,12 +1,15 @@
-#!/usr/bin/python3
 
-import random, syslog
+from elasticsearch import Elasticsearch
+import random
 from const import PORTMAP
 from sys import exit
 from time import sleep
 
-def main():
+#connect to the server
+elastic_client= Elasticsearch('http://localhost:9200')
 
+def main():
+    
     port_list = []
     type_attack_list = []
 
@@ -21,28 +24,43 @@ def main():
                                 random.randrange(1,2000),
                                 random.randrange(100,1000)
                                 )
+        
+        #define the query 
+        query_body = 
+        {
+            "query":{
+                "range":{
+                    "@timestamp":{
+                        "gte":"2015-08-04T11:00:00",
+                        "lt":"2015-08-04T12:00:00"
+                    }
+                }
+            }
+        }
+        
+        id = randint(0,100)
+        result = elastic_client.search(index='logstash-2022.07.13-000008', body=query_body, id= id)
 
-        rand_data = '{}.{}.{}.{},{}.{}.{}.{},{},{},{},{}'.format(
-                                                            random.randrange(1, 256),
-                                                            random.randrange(1, 256),
-                                                            random.randrange(1, 256),
-                                                            random.randrange(1, 256),
-                                                            random.randrange(1, 256),
-                                                            random.randrange(1, 256),
-                                                            random.randrange(1, 256),
-                                                            random.randrange(1, 256),
-                                                            port,
-                                                            port,
+        try:
+
+            attack_data = '{},{},{},{},{},{}'.format(
+                                                            attack_doc["_source"]["src_ip"],
+                                                            attack_doc["_source"]["dest_ip"],
+                                                            attack_doc["_source"]["src_port"],
+                                                            attack_doc["_source"]["dest_port"],
                                                             type_attack,
                                                             cve_attack
                                                             )
-
-        syslog.syslog(rand_data)
-        print(rand_data)
+        except :
+            pass
+        
+       syslog(attack_data)
+        print(attack_data)
         sleep(1)
 
 if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
-        exit()
+        exit() 
+
